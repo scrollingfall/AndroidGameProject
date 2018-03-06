c6dd21867c8980da2102f26d4ce13a78a1f65d5e
Jerry Chen <strangejerrychen@gmail.com>
2018-03-06 13:22:22 -0800

    backend 1

--- app/src/main/java/edu/stanford/cs108/bunnyworldplayer/Game.java
+++ app/src/main/java/edu/stanford/cs108/bunnyworldplayer/Game.java
@@ -1,9 +1,34 @@
 package edu.stanford.cs108.bunnyworldplayer;
 
+import java.util.ArrayList;
+
 /**
  * Created by Jerry Chen on 3/5/2018.
  */
 
 public class Game {
-    // hi. i made changes
+    private String name;
+    private boolean valid = false; //might need to be appropriately updated upon adding anything
+    private ArrayList<Page> pages;
+    private Page posessions;
+    private ArrayList<String> resources;
+
+    public Game(String name) {
+        this.name = name;
+        pages = new ArrayList<Page>();
+        posessions = new Page("posessions", 100, 100); //random values
+    }
+
+    private void initResources() {
+        resources = new ArrayList<String>();
+        //todo add all resources
+    }
+
+    public boolean addPage(Page p) {
+        //error checking
+        pages.add(p);
+        return true;
+    }
+
+    //todo parsing collection
 }
--- app/src/main/java/edu/stanford/cs108/bunnyworldplayer/Shape.java
+++ app/src/main/java/edu/stanford/cs108/bunnyworldplayer/Shape.java
@@ -5,4 +5,42 @@ package edu.stanford.cs108.bunnyworldplayer;
  */
 
 public class Shape {
+    private String name;
+    private String owner;
+    private String text;
+    private String image;
+    private boolean hidden;
+    private boolean moveable;
+    private float x;
+    private float y;
+    private float width;
+    private float height;
+    private boolean inBackpack;
+    public Shape (String name, float x, float y, float width, float height) {
+        this.name = name;
+        this.owner = "";
+        this.text = "";
+        this.image = "";
+        this.hidden = true;
+        this.moveable = false;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.inBackpack = false;
+    }
+    public Shape (String name, String owner, String text, String image, boolean hidden, boolean moveable,
+                  float x, float y, float width, float height, boolean inBackpack) {
+        this.name = name;
+        this.owner = owner;
+        this.text = text;
+        this.image = image;
+        this.hidden = hidden;
+        this.moveable = moveable;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.inBackpack = inBackpack;
+    }
 }
