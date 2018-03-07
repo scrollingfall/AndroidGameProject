9c3cfa0fdc85a5d4ac7070850e5e6702d2c58521
vsinghi <vsinghi@stanford.edu>
2018-03-06 21:20:43 -0800

    made some changes in the main activity page

--- app/src/main/java/edu/stanford/cs108/bunnyworldplayer/MainActivity.java
+++ app/src/main/java/edu/stanford/cs108/bunnyworldplayer/MainActivity.java
@@ -1,10 +1,19 @@
 package edu.stanford.cs108.bunnyworldplayer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    private void openFile(File file){
+        //read lines
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,13 +22,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void createGame(View view){
-
+        Intent intent = new Intent(this, Editor.class);
+        startActivity(intent);
     }
 
     public void editGame(View view){
 
+        Intent intent = new Intent(this, Editor.class);
+        startActivity(intent);
     }
+
     public void playGame(View view){
 
+
+        Intent intent = new Intent(this, Game.class);
+        startActivity(intent);
     }
 }
