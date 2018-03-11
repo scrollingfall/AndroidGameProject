980afb65d51b6c838fb14e7c49e17de1609bda81
Sam Kwong <samkwong@stanford.edu>
2018-03-10 22:08:17 -0800

    fixed Shape.java

--- app/src/main/java/edu/stanford/cs108/bunnyworldplayer/Shape.java
+++ app/src/main/java/edu/stanford/cs108/bunnyworldplayer/Shape.java
@@ -36,6 +36,7 @@ public class Shape extends RectF {
     private float y;
     private float width;
     private float height;
+    private int fontSize;
     private boolean inBackpack;
     private String onClick;
     private String onEnter;
@@ -47,15 +48,18 @@ public class Shape extends RectF {
     public String transitionPage = "";
     public ArrayList<String> actionShowShapes = new ArrayList<String>();
     public ArrayList<String> actionHideShapes = new ArrayList<String>();
+    public static int shapeID;
 
 
     // Constructor
-    public Shape (Context context, String name, float x, float y, float width, float height) {
+    public Shape (Context context, String name, String owner, float x, float y, float width, float height) {
+        this.shapeID = getPreviousShapeId() + 1;
+        setPreviousShapeId(shapeID);
         this.name = name;
-        this.owner = "";
+        this.owner = owner;
         this.text = "";
         this.image = "";
-        this.hidden = true;
+        this.hidden = false;
         this.moveable = false;
         this.x = x;
         this.y = y;
@@ -67,32 +71,41 @@ public class Shape extends RectF {
         this.onDrop = new HashMap<String, String>();
         this.editorMode = false;
         this.context = context;
+        this.fontSize = 20;
+
     }
+    public int getPreviousShapeId(){ return shapeID;}
+    private void setPreviousShapeId(int shapeID) { this.shapeID = shapeID; }
+    public int getShapeId() { return shapeID; }
 
     public void setEditorMode (boolean editable){
         editorMode = editable;
     }
 
-    public Shape (Context context, String name, String owner, String text, String image, boolean hidden, boolean moveable,
-                  float x, float y, float width, float height, boolean inBackpack) {
-        this.name = name;
-        this.owner = owner;
-        this.text = text;
-        this.image = image;
-        this.hidden = hidden;
-        this.moveable = moveable;
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.inBackpack = inBackpack;
-        this.onClick = "";
-        this.onEnter = "";
-        this.onDrop = new HashMap<String, String>();
-        this.editorMode = false;
-        this.context = context;
-    }
+//    public Shape (Context context, String name, String owner, String text, String image, boolean hidden, boolean moveable,
+//                  float x, float y, float width, float height, boolean inBackpack) {
+//        this.shapeID = getPreviousShapeId() + 1;
+//        setPreviousShapeId(shapeID);
+//        this.name = name;
+//        this.owner = owner;
+//        this.text = text;
+//        this.image = image;
+//        this.hidden = hidden;
+//        this.moveable = moveable;
+//        this.x = x;
+//        this.y = y;
+//        this.width = width;
+//        this.height = height;
+//        this.inBackpack = inBackpack;
+//        this.onClick = "";
+//        this.onEnter = "";
+//        this.onDrop = new HashMap<String, String>();
+//        this.editorMode = false;
+//        this.context = context;
+//        this.fontSize = 20;
+//    }
 
+    public int getFontSize(){ return fontSize;}
     public String getName() {
         return name;
     }
@@ -255,7 +268,7 @@ public class Shape extends RectF {
             ArrayList<String> scriptWords = new ArrayList<>();
             StringTokenizer st = new StringTokenizer(script, " ");
             while (st.hasMoreTokens()) scriptWords.add(st.nextToken());
-            String triggerWords = scriptWords.get(0) + " " scriptWords.get(1);
+            String triggerWords = scriptWords.get(0) + " " + scriptWords.get(1);
             scriptWords.remove(scriptWords.get(0));
             scriptWords.remove(scriptWords.get(0));
             if (triggerWords.equals("on drop")){
@@ -290,8 +303,7 @@ public class Shape extends RectF {
             } else if (words[0].equals("hide")) actionHideShapes.add(words[1]);
             else if (words[0].equals("show")) actionShowShapes.add(words[1]);
             else if (words[0].equals("play")) {
-                MediaPlayer soundPlayer = MediaPlayer.create(context, context.getResources().getIdentifier(words[1], "raw", context.getPackageName());)
-                ;
+                MediaPlayer soundPlayer = MediaPlayer.create(context, context.getResources().getIdentifier(words[1], "raw", context.getPackageName()));
                 soundPlayer.start();
 
                 // Citation: https://stackoverflow.com/questions/24326269/setoncompletionlistener-mediaplayer-oncompletionlistener-in-the-type-mediaplay
