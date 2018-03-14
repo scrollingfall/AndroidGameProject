54aa0d737570949c8c0818714df3bbd4ab0f4d6f
Jerry Chen <strangejerrychen@gmail.com>
2018-03-13 20:00:16 -0700

    size code

---
+++ app/src/main/java/edu/stanford/cs108/bunnyworldplayer/PlayerActivity.java
@@ -0,0 +1,17 @@
+package edu.stanford.cs108.bunnyworldplayer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PlayerActivity extends Activity {
+
+    private PlayerView player;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+        player = (PlayerView)findViewById(R.id.playerView);
+        //player.setGame(<game passed in from intent>) important this goes before the next line
+        player.setSize(player.getWidth(), player.getHeight());
+    }
+}
